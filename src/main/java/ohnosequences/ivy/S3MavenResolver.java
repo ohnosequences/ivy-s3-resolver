350173804f3959c475477844487d0f4d3b9eeca3
evdokim <museeer@gmail.com>
2013-08-12 18:40:20 +0200

    attempt to put S3 resolving functionality to ivy-s3-resolver

---
+++ src/main/java/ohnosequences/ivy/S3MavenResolver.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2004-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ohnosequences.ivy;
+
+import org.apache.ivy.plugins.resolver.IBiblioResolver;
+import org.apache.ivy.plugins.resolver.RepositoryResolver;
+
+/**
+ * A dependency resolver that looks to an S3 repository to resolve dependencies.
+ * 
+ * @author Ben Hale
+ * @author Evdokim Kovach
+ */
+public class S3MavenResolver extends IBiblioResolver {
+	
+	public void setAccessKey(String accessKey) {
+		((S3Repository)getRepository()).setAccessKey(accessKey);
+	}
+	
+	public void setSecretKey(String secretKey) {
+		((S3Repository)getRepository()).setSecretKey(secretKey);
+	}
+
+	public S3MavenResolver() {
+		setRepository(new S3Repository());
+	}
+
+	public String getTypeName() {
+		return "s3";
+	}
+}
